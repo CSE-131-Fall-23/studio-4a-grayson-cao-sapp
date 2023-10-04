b7a79a20dec7ae989c73c8ecc058b21fa74b5b2b
maddiegrayson <maddiegrayson@localhost>
2023-10-04 15:01:53 -0500

    Help my java stopped working

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,18 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		// TODO: 4. Iterate through all the data and display it using StdDraw
+		StdDraw.setPenRadius(0.008);
+		StdDraw.setPenColor(StdDraw.GREEN);
 	}
 }
